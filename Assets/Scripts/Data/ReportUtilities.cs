4851cc29fcca63542bce2d80658eeac9a592c3b6
Owen Magelssen <44145090+OwenMagelssen@users.noreply.github.com>
2023-02-21 20:00:12 -0800

    initial TableLayout asset integration

--- Assets/Scripts/Data/ReportUtilities.cs
+++ Assets/Scripts/Data/ReportUtilities.cs
@@ -1,47 +1,59 @@
-using System.Globalization;
-using System.IO;
-using CsvHelper;
-using CsvHelper.Configuration;
-using UnityEditor;
 using UnityEngine;
+using TMPro;
+using Unity.VisualScripting;
 
 namespace SchedulingUtilities
 {
     public static class ReportUtilities
     {
-        [MenuItem("Assets/Create/Scheduling Time Off Request Table", priority = 50)]
-        public static void CsvToTableGUI()
+        public static RectTransform CreateStringCell(string value)
         {
-            string selectionPath = AssetDatabase.GetAssetPath(Selection.activeInstanceID);
-            FileAttributes attr = File.GetAttributes(selectionPath);
-            bool isDirectory = (attr & FileAttributes.Directory) == FileAttributes.Directory;
-
-            if (string.IsNullOrEmpty(selectionPath) || isDirectory)
-            {
-                Debug.Log("A CSV file must be selected to generate a table.");
-                return;
-            }
-            
-            var csvConfig = new CsvConfiguration(CultureInfo.CurrentCulture)
-            {
-                HasHeaderRecord = false
-            };
-
-            Debug.Log(selectionPath);
-            using var streamReader = File.OpenText(selectionPath);
-            using var csvReader = new CsvReader(streamReader, csvConfig);
-
-            while (csvReader.Read())
-            {
-                string row = string.Empty;
-                
-                for (int i = 0; csvReader.TryGetField(i, out string value); i++)
-                {
-                    row += value;
-                }
-
-                Debug.Log(row);
-            }
+            var go = new GameObject();
+            var rt = go.AddComponent<RectTransform>();
+            rt.pivot = Vector2.zero;
+            rt.anchorMin = Vector2.zero;
+            rt.anchorMax = Vector2.one;
+            rt.sizeDelta = Vector2.zero;
+            var text = rt.AddComponent<TextMeshProUGUI>();
+            text.text = value;
+            return rt;
         }
+        
+        
+        
+        // [MenuItem("Assets/Create/Scheduling Time Off Request Table", priority = 50)]
+        // public static void CsvToTableGUI()
+        // {
+        //     string selectionPath = AssetDatabase.GetAssetPath(Selection.activeInstanceID);
+        //     FileAttributes attr = File.GetAttributes(selectionPath);
+        //     bool isDirectory = (attr & FileAttributes.Directory) == FileAttributes.Directory;
+        //
+        //     if (string.IsNullOrEmpty(selectionPath) || isDirectory)
+        //     {
+        //         Debug.Log("A CSV file must be selected to generate a table.");
+        //         return;
+        //     }
+        //     
+        //     var csvConfig = new CsvConfiguration(CultureInfo.CurrentCulture)
+        //     {
+        //         HasHeaderRecord = false
+        //     };
+        //
+        //     Debug.Log(selectionPath);
+        //     using var streamReader = File.OpenText(selectionPath);
+        //     using var csvReader = new CsvReader(streamReader, csvConfig);
+        //
+        //     while (csvReader.Read())
+        //     {
+        //         string row = string.Empty;
+        //         
+        //         for (int i = 0; csvReader.TryGetField(i, out string value); i++)
+        //         {
+        //             row += value;
+        //         }
+        //
+        //         Debug.Log(row);
+        //     }
+        // }
     }
 }
--- Assets/Scripts/GUI/TestGUI.cs
+++ Assets/Scripts/GUI/TestGUI.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Globalization;
+using UI.Tables;
 using UnityEngine;
 
 namespace SchedulingUtilities
@@ -6,10 +8,37 @@ namespace SchedulingUtilities
     public class TestGUI : MonoBehaviour
     {
         public TimeOffRequestReport report;
+        public TableLayout tableLayout;
 
-        public void OnGUI()
+        private void Start()
         {
             if (report == null) return;
+            if (tableLayout == null) return;
+            tableLayout.ClearRows();
+            var culture = new CultureInfo("en-US");
+
+            foreach (var request in report.timeOffRequests)
+            {
+                var row = tableLayout.AddRow();
+                row.preferredHeight = 128;
+                row.AddCell(ReportUtilities.CreateStringCell(request.EmployeeName));
+                row.AddCell(ReportUtilities.CreateStringCell(request.JobTitle.ToString()));
+                row.AddCell(ReportUtilities.CreateStringCell(request.TimeOffStart.ToString(culture)));
+                row.AddCell(ReportUtilities.CreateStringCell(request.Hours.ToString(culture)));
+                row.AddCell(ReportUtilities.CreateStringCell(request.RequestedOn.ToString(culture)));
+                row.AddCell(ReportUtilities.CreateStringCell(request.Status.ToString()));
+            }
+
+            float width = (float)Screen.width / tableLayout.ColumnWidths.Count;
+            
+            for (int i = 0; i < tableLayout.ColumnWidths.Count; i++)
+                tableLayout.ColumnWidths[i] = width;
+            
+            tableLayout.CalculateLayoutInputHorizontal();
+        }
+
+        private void DoImmediateModeGUI()
+        {
             var culture = new CultureInfo("en-US");
             
             foreach (var request in report.timeOffRequests)
@@ -24,5 +53,12 @@ namespace SchedulingUtilities
                 GUILayout.EndHorizontal();
             }
         }
+
+        // public void OnGUI()
+        // {
+        //     if (report == null) return;
+        //     if (tableLayout == null) return;
+        //     
+        // }
     }
 }
