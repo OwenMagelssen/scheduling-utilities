fd7e0a5b2b1ea3fc0938b909196c5bb630bba243
Owen Magelssen <44145090+OwenMagelssen@users.noreply.github.com>
2023-02-26 13:09:43 -0800

    added time off request selection in table

--- Assets/Scripts/GUI/TestGUI.cs
+++ Assets/Scripts/GUI/TestGUI.cs
@@ -4,6 +4,7 @@ using System.Globalization;
 using UI.Tables;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 namespace SchedulingUtilities
 {
@@ -13,9 +14,30 @@ namespace SchedulingUtilities
         public TimeOffRequestReport report;
         public TableLayout tableLayout;
         public Vector2 padding = Vector2.one * 24.0f;
+        [Header("Row Selection")]
+        public ColorBlock rowSelectableColors = new ColorBlock
+        {
+            normalColor = Color.white,
+            highlightedColor = new Color(130, 200, 255, 255),
+            pressedColor = new Color(151, 146, 255, 255),
+            selectedColor = new Color(59, 69, 255, 255),
+            disabledColor = new Color(173, 173, 173, 255),
+            colorMultiplier = 1.0f,
+            fadeDuration = 0.2f
+        };
 
         private RectTransform _rectTransform;
         private List<TextMeshProUGUI> _allCellTexts = new();
+
+        private void Awake()
+        {
+            _rectTransform = GetComponent<RectTransform>();
+        }
+
+        private void Start()
+        {
+            CreateTable();
+        }
 
         private float AddStringCellGetWidth(TableRow row, TextMeshProUGUI text, string value)
         {
@@ -25,28 +47,24 @@ namespace SchedulingUtilities
             rt.anchorMin = Vector2.zero;
             rt.anchorMax = Vector2.one;
             rt.sizeDelta = Vector2.zero;
+            row.AddCell(rt);
             text.text = value;
             text.ForceMeshUpdate();
-            row.AddCell(rt);
             return text.preferredWidth;
         }
 
-        private void Awake()
-        {
-            _rectTransform = GetComponent<RectTransform>();
-        }
-
-        private void Start()
+        private void CreateTable()
         {
             if (report == null) return;
             if (tableLayout == null) return;
             tableLayout.ClearRows();
+            tableLayout.RowBackgroundColor = Color.white;
             var culture = new CultureInfo("en-US");
             var columnWidths = new List<float>() { 0, 0, 0, 0, 0, 0 };
 
             foreach (var request in report.timeOffRequests)
             {
-                var row = tableLayout.AddRow();
+                TableRow row = tableLayout.AddRow();
                 columnWidths[0] = Mathf.Max(columnWidths[0], AddStringCellGetWidth(row, Instantiate(stringCellPrefab), request.EmployeeName));
                 columnWidths[1] = Mathf.Max(columnWidths[1], AddStringCellGetWidth(row, Instantiate(stringCellPrefab), request.JobTitle.ToString()));
                 columnWidths[2] = Mathf.Max(columnWidths[2], AddStringCellGetWidth(row, Instantiate(stringCellPrefab), request.TimeOffStart.ToString(culture)));
@@ -72,10 +90,24 @@ namespace SchedulingUtilities
             float tableHeight = 0;
             
             for (int i = 0; i < tableLayout.Rows.Count; i++)
-                tableHeight += tableLayout.Rows[i].actualHeight;
+                tableHeight += tableLayout.Rows[i].preferredHeight;
 
             var rt = tableLayout.GetComponent<RectTransform>();
             rt.sizeDelta = new Vector2(rt.sizeDelta.x, tableHeight);
+
+            for (int i = 0; i < tableLayout.Rows.Count; i++)
+            {
+                var button = tableLayout.Rows[i].gameObject.AddComponent<Button>();
+                var selectable = button as Selectable;
+                selectable.colors = rowSelectableColors;
+                int index = i;
+                button.onClick.AddListener(() => SelectRequest(report.timeOffRequests[index]));
+            }
+        }
+
+        private void SelectRequest(TimeOffRequest request)
+        {
+            
         }
 
         private void DoImmediateModeGUI()
