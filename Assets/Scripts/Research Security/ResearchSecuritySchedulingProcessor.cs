28f751492ab8b705bd04a44c9ed5199c0f7794e3
Owen Magelssen <44145090+OwenMagelssen@users.noreply.github.com>
2023-08-18 20:44:52 -0700

    laying out data from scheduling requests

--- Assets/Scripts/Research Security/ResearchSecuritySchedulingProcessor.cs
+++ Assets/Scripts/Research Security/ResearchSecuritySchedulingProcessor.cs
@@ -2,6 +2,16 @@ namespace SchedulingUtilities.Research_Security
 {
 	public class ResearchSecuritySchedulingProcessor
 	{
-		
+		// cells are 
+		// int - unique request id (sequential integers)
+		// datetime - start time
+		// datetime - completion time
+		// string - email
+		// string - name first last
+		// datetime last modified time
+		// int - employee id
+		// what type of scheduling request
+		// date of shift
+		// shift (day/swing/night)
 	}
 }
